9f156a4ce2bbc1256d37aea09260ec64425e9127
Irina Shmigelskaya <shmigelskaya.irina2016@yandex.ru>
2020-11-26 23:03:00 +0300

    swiper styles

--- src/about/index.js
+++ src/about/index.js
@@ -2,36 +2,8 @@ import GithubApi from '../js/modules/GithubApi.js';
 import CommitCard from '../js/components/CommitCard.js';
 import CommitCardList from '../js/components/CommitCardList.js';
 
-// core version + navigation, pagination modules:
-import Swiper, { Navigation, Pagination  } from 'swiper';
-
-// import Swiper styles
 import 'swiper/swiper-bundle.css';
-
 import "../css-pages/about-page.css";
-
-// configure Swiper to use modules
-Swiper.use([Navigation, Pagination]);
-
-// init Swiper:
-const swiper = new Swiper('.swiper-container', {
-    direction: 'horizontal',
-    speed: 400,
-    spaceBetween: 100,
-  
-    pagination: {
-      el: '.swiper-pagination',
-    },
-  
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
-});
-
-const mySwiper = document.querySelector('.swiper-container').swiper;
-/*mySwiper = slideNext();
-mySwiper = slidePrev();*/
 
 const swiperWrapper = document.querySelector('.swiper-wrapper');
 const commitCard = new CommitCard();
@@ -45,11 +17,36 @@ function requestUserRepos() {
         })
         .catch((err) => {
             console.log(`ошибка запроса ${err}`);
+        })
+        .then(() => {
+          const swiper = new Swiper('.swiper-container', {
+            direction: 'horizontal',
+            slidesPerVew: 3,
+            speed: 400,
+            spaceBetween: 100,
+            centeredSlides: true,
+          
+            pagination: {
+              el: '.swiper-pagination',
+              clickable: true,
+            },
+          
+            navigation: {
+              nextEl: '.swiper-button-next',
+              prevEl: '.swiper-button-prev',
+            },
+          })
         })   
 };
 
-//создаем параметры запроса
 const baseUrl = `https://api.github.com/repos/`;
 const githubApi = new GithubApi(baseUrl);
 
 requestUserRepos();
+
+import Swiper, { Navigation, Pagination  } from 'swiper';
+Swiper.use([Navigation, Pagination]);
+
+const mySwiper = document.querySelector('.swiper-container').swiper;
+/*mySwiper = slideNext();
+mySwiper = slidePrev();*/
